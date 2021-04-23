c74c8a7ac625a431408f9c19ab5dd18fe55a5dc3
Riya Shrestha <78842522+Ree3a@users.noreply.github.com>
2021-04-23 14:16:29 +0545

    Add files via upload

---
+++ ques3.py
@@ -0,0 +1,7 @@
+'''3. Check whether the user input number is even or odd and display it to user. '''
+
+number = int(input('Enter the number:'))
+if number % 2 == 0:
+    print('This is an even number')
+else:
+    print('This is odd number')
